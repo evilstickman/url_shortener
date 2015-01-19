c1950672cb5b463ee5f8ba62ed40d7291358733f
Matt Billock <mattbillock@mattbillock.com>
2015-01-18 20:18:37 -0600

    Added model tests. Also converted to expects method to eliminate warnings

--- spec/lib/helpers/url_helper_spec.rb
+++ spec/lib/helpers/url_helper_spec.rb
@@ -10,11 +10,11 @@ describe UrlHelper do
     end
     
     it "generates an 8-character string" do
-      @url_helper.generate_url_token().length.should eq 8
+      expect(@url_helper.generate_url_token().length).to eq 8
     end
     
     it "generates a string with only hex characters" do
-      @url_helper.generate_url_token().should match(/(a-f0-9)*/)
+      expect(@url_helper.generate_url_token()).to match(/(a-f0-9)*/)
     end
   end
 
@@ -26,13 +26,13 @@ describe UrlHelper do
 
     it "should try to find a unique string if the first string already exists" do
       allow(ShortenedUrl).to receive(:exists?).and_return(true, false)
-      @url_helper.should receive(:generate_url_token).twice
+      expect(@url_helper).to receive(:generate_url_token).twice
       @url_helper.get_unique_url_token
     end
 
     it "should return immediately if the generated token doesn't exist" do
       allow(ShortenedUrl).to receive(:exists?).and_return(false)
-      @url_helper.should receive(:generate_url_token).once
+      expect(@url_helper).to receive(:generate_url_token).once
       @url_helper.get_unique_url_token
     end
   end
--- spec/models/shortened_url_test.rb
+++ spec/models/shortened_url_test.rb
@@ -4,4 +4,8 @@ class ShortenedUrlTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  it "should allow searching by url tokens" do
+    assert_includes UrlHelper.find.where({shortened_url: "abcdef12"}), shortened_urls(:google)
+    refute_includes UrlHelper.find.where({shortened_url: "abcdef34"}), shortened_urls(:google)
+  end
 end
